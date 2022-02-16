106641d53eeca4d0616ad91287611be4ed699f29
Erick Ferrareto <erickskate38@gmail.com>
2022-02-15 22:23:34 -0300

    Second commit

--- src/teste.kt
+++ src/teste.kt
@@ -1,3 +1,21 @@
 fun main(){
-    print("Hello Kotlin");
+    val x = 3;
+    var y = 10;
+    y = 5;
+    print(x + y);
+
+    // Função que não retorna nada.
+    fun function(): Unit {
+        val result = 2 + 2;
+        print(result);
+    }
+    function();
+
+    // Função com returno
+    fun function2(x: Int , y: Int): Int{
+        val result = x + y;
+        return result;
+    }
+    print(function2(2, 7))
+
 }
