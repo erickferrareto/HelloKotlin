7a8f77aa6eb09783a42a5f094d1ce1fdc3d63fcd
Erick Ferrareto <erickskate38@gmail.com>
2022-02-17 22:46:24 -0300

    Second commit

--- src/teste.kt
+++ src/teste.kt
@@ -1,21 +1,34 @@
-fun main(){
+fun main() {
     val x = 3;
     var y = 10;
     y = 5;
-    print(x + y);
+    println(x + y);
 
     // Função que não retorna nada.
     fun function(): Unit {
         val result = 2 + 2;
-        print(result);
+        println(result);
     }
     function();
 
     // Função com returno
-    fun function2(x: Int , y: Int): Int{
+    fun function2(x: Int, y: Int): Int {
         val result = x + y;
         return result;
     }
-    print(function2(2, 7))
+    println(function2(2, 7))
 
+    //Condições
+    val idade = 18;
+    val senha = 594428;
+    val correto = 594428;
+    if (idade >= 18 && senha == correto){
+        println("Hello World");
+    } else if (idade < 18 && senha == correto){
+        println("Error World")
+    } else if (idade == 18 && senha != correto){
+        println("Hello Error")
+    } else if (idade < 18 && senha != correto){
+        println("Error Error")
+    }
 }
