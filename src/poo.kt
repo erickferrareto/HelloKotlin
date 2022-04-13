88797c00f524a12240e98b1cfdda8e571afc416b
Erick Ferrareto <erickskate38@gmail.com>
2022-04-12 22:44:21 -0300

    commit

---
+++ src/poo.kt
@@ -0,0 +1,24 @@
+//Porgramção Orientada a Objetos em Kotlin
+open class Person(var name : String, var old : Int){
+    init {
+        this.name = name.toUpperCase()
+        println(name)
+    }
+}
+class Man(name : String, old : Int) : Person(name, old){
+    init {
+        println("O $name possui $old anos e é homem")
+    }
+}
+
+class Woman(name : String, old : Int) : Person(name, old){
+    init {
+        println("A $name possui $old anos e é mulher")
+    }
+}
+
+fun main(){
+    var pessoa1 = Man("Erick", 22)
+    var pessoa2 = Woman("Tayna", 24)
+    var pessoa3 = Person("Pedro", 14)
+}
